f8073b5325d1993789664f8818b79a659952b9df
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-03 18:29:18 +0200

    Errores de importacion arreglados

--- js/personajes.js
+++ js/personajes.js
@@ -1,10 +1,7 @@
-import { Rey } from "./rey.js";
-import { Luchador } from "./luchador.js";
-import { Asesor } from "./asesor.js";
-import { Escudero } from "./escudero.js";
-
-
-
+import Rey from "./rey.js";
+import Luchador from "./luchador.js";
+import Asesor from "./asesor.js";
+import Escudero from "./escudero.js";
 
 const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
 const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
@@ -12,4 +9,4 @@ const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
 const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
 const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
 
-export default const personajes = [joffrey, jaime, tyrion, daenerys, bronn];
+export const personajes = [joffrey, jaime, tyrion, daenerys, bronn];
